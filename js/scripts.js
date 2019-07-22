b190c39e85ec9c3d87f78dcbcd7013b88fe1a17e
michael760odhiambo <michaelodhiambo760@gmail.com>
2019-07-22 10:29:27 +0300

    add jquery effects to about us icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,40 @@
-$(document).ready(function(){
-    $("#sec1").click(function(){
-        $("#sec1").toggle();
-        $("#sec2").toggle();
-    })
-})
 
-(document).ready(function(){
-    $("#sec2").click(function(){
-        $("#sec1").toggle();
-        $("#sec2").toggle();
-    })
-})
+ 
+$(document).ready(function(){
+    $("#icons1").click(function(){
+        $("#icons1").hide();
+        $("#hiden1").show();
+    });
+});
+$(document).ready(function(){
+    $("#hiden1").click(function(){
+        $("#icons1").show();
+        $("#hiden1").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#icons2").click(function(){
+        $("#icons2").hide();
+        $("#hiden2").show();
+    });
+});
+$(document).ready(function(){
+    $("#hiden2").click(function(){
+        $("#icons2").show();
+        $("#hiden2").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#icons3").click(function(){
+        $("#icons3").hide();
+        $("#hiden3").show();
+    });
+});
+$(document).ready(function(){
+    $("#hiden3").click(function(){
+        $("#icons3").show();
+        $("#hiden3").hide();
+    });
+});
